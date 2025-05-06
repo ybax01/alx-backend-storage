3c24e24ecc888963c1f30e787d09e3642ac4e806
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-05-06 17:03:00 +0100

    9th

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Task 9
+'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''
+    Inserts a new document in a collection
+    '''
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
